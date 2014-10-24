8cd93bd49b1a573d0c8c72776ec9e6db6f750697
shuson <wlgq@hotmail.com>
2014-10-24 21:33:24 +0800

    init and deploy
    complete simple version

--- Temperature Converter/ViewController.swift
+++ Temperature Converter/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var kelvinLabel: UILabel!
+    @IBOutlet weak var temperatureSlider: UISlider!
+    
+    
+    @IBAction func temperatureSliderAction(sender: AnyObject) {
+        self.updateTemperatureDisplays()
+    }
+    
+    func updateTemperatureDisplays(){
+        var currentC = floor(temperatureSlider.value)
+        var currentF = Double(currentC)*(9/5)+32
+        var currentK = Double(currentC) + 273.15
+
+        celsiusLabel.text = String(format: "%.fC", currentC)
+        fahrenheitLabel.text = String(format: "%.1fF", currentF)
+        kelvinLabel.text = String(format: "%.2fK",currentK )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
